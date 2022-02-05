9cd5cf628c4d27e7ec676074ebfa8c1b7c0e1245
Hathenia <Nick1234567890a@gmail.com>
2022-02-05 18:57:07 -0500

    First commit

---
+++ VideoPlayer/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using VideoPlayer.ViewModels;
+using VideoPlayer.Views;
+using Xamarin.Forms;
+
+namespace VideoPlayer
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            //Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            //Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
